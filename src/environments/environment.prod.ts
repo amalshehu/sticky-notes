33304cec541c2bfdfb39e6c7c6e17a6c3fb158a9
amalshehu <amalshehu@gmail.com>
2017-05-21 17:50:37 +0530

    Add precache

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+   firebase: {
+    apiKey: 'AIzaSyAin5DbqVpuaC64ymdfGvd8McTXC9U3uVc',
+    authDomain: 'sticky-notes-c7b4f.firebaseapp.com',
+    databaseURL: 'https://sticky-notes-c7b4f.firebaseio.com',
+    projectId: 'sticky-notes-c7b4f',
+    storageBucket: 'sticky-notes-c7b4f.appspot.com',
+    messagingSenderId: '149288624175'
+  }
 };
