4c29a6c62733751d0d52dbfe46bddb028b1a8291
amalshehu <amalshehu@gmail.com>
2017-05-24 23:58:11 +0530

    Modify login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,25 @@
+import { AppService } from '../app.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user: any;
 
-  constructor() { }
+  constructor(public service: AppService) { }
 
   ngOnInit() {
+    this.service.userData.subscribe(
+      (data: any) => {
+        console.log(data);
+        // debugger
+        this.user = data;
+      }
+    );
   }
 
 }
